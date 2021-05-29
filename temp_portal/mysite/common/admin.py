918bc113bbdb23c8cc96d8a336bb56d752afb39f
rodom(notebook) <enjoying1081@naver.com>
2021-05-29 13:23:58 +0900

    login+createaccount. remove some bug

---
+++ temp_portal/mysite/common/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin) :
+    list_display=('username','first_name','last_name','job', 'is_staff')
+    list_filter=('job',)
---
+++ temp_portal/mysite/common/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import UserForm
+
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            """username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            user = authenticate(username=username, password=raw_password, first_name=first_name, last_name = last_name)
+            login(request, user)"""
+            return redirect('/')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
