9b11a4e06d5e79f694bb9575e6c3effc8b33eb26
RYUWANWOO <42319065+RYUWANWOO@users.noreply.github.com>
2021-05-29 20:06:53 +0900

    add class and merge

--- temp_portal/mysite/config/urls.py
+++ temp_portal/mysite/config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from pybo import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('',include('common.urls')),
     path('index/',views.index,name='index'),
+    path('class/',include('class.urls')),
+
 ]
